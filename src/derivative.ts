fbfec93e8d48d8d1c3e69dc0ddb69c85be28ed5c
an-Iceberg <a.g.marosi@bluewin.ch>
2025-05-15 22:24:52 +0200

    Programmatically more ergonomic implementation of the derivative

--- src/derivative.ts
+++ src/derivative.ts
@@ -1,22 +1,33 @@
-export function d1(f: (x: number) => number, x: number, h: number = 0.001): number
+// https://en.wikipedia.org/wiki/Stencil_(numerical_analysis)
+
+export function d(f: (x: number) => number, x: number, h: number = 0.001): number
 {
   return (f(x - 2 * h) - 8 * f(x - h) + 8 * f(x + h) - f(x + 2 * h)) / (12 * h);
 }
 
 export function d2(f: (x: number) => number, x: number, h: number = 0.001): number
 {
-  return (-f(x - 2 * h) + 16 * f(x - h) - 30 * f(x) + 16 * f(x + h) - f(x + 2 * h)) / (12
-    * Math.pow(h, 2));
+  return d((x) => d(f, x, h), x, h);
+
+  // return (-f(x - 2 * h) + 16 * f(x - h) - 30 * f(x) + 16 * f(x + h) - f(x + 2 * h)) / (12
+  //   * Math.pow(h, 2));
+
+  // return (-(1 / 12) * f(x - 2 * h) + (4 / 3) * f(x - h) - 2.5 * f(x) + (4 / 3) * f(x + h)
+  //   - (1 / 12) * f(x + 2 * h)) / Math.pow(h, 2);
 }
 
 export function d3(f: (x: number) => number, x: number, h: number = 0.001): number
 {
-  return (-f(x - 2 * h) + 2 * f(x - h) - 2 * f(x + h) + f(x + 2 * h)) / (2 * Math.pow(h, 3));
+  return d((x) => d2(f, x, h), x, h);
+
+  // return (-f(x - 2 * h) + 2 * f(x - h) - 2 * f(x + h) + f(x + 2 * h)) / (2 * Math.pow(h, 3));
 }
 
 export function d4(f: (x: number) => number, x: number, h: number = 0.001): number
 {
-  return (f(x - 2 * h) - 4 * f(x - h) + 6 * f(x) - 4 * f(x + h) + f(x + 2 * h)) / (Math.pow(h, 4));
+  return d((x) => d3(f, x, h), x, h);
+
+  // return (f(x - 2 * h) - 4 * f(x - h) + 6 * f(x) - 4 * f(x + h) + f(x + 2 * h)) / (Math.pow(h, 4));
 }
 
 {
@@ -27,7 +38,7 @@ export function d4(f: (x: number) => number, x: number, h: number = 0.001): numb
   const d3_f = (x: number) => Math.exp(x);
   const d4_f = (x: number) => Math.exp(x);
 
-  console.log(`d1 ε = ${Math.abs(d1_f(x) - d1(f, x)).toExponential(2)}`);
+  console.log(`d1 ε = ${Math.abs(d1_f(x) - d(f, x)).toExponential(2)}`);
   console.log(`d2 ε = ${Math.abs(d2_f(x) - d2(f, x)).toExponential(2)}`);
   console.log(`d3 ε = ${Math.abs(d3_f(x) - d3(f, x)).toExponential(2)}`);
   console.log(`d4 ε = ${Math.abs(d4_f(x) - d4(f, x)).toExponential(2)}`);
--- src/integral.ts
+++ src/integral.ts
@@ -43,7 +43,7 @@ export function int_3(
   // deno-fmt-ignore
   return (3/8)*h*range(1, n/3)
     .map(i => f(x(3*i - 3)) + 3*f(x(3*i - 2)) + 3*f(x(3*i - 1)) + f(x(3*i)))
-    .reduce((prev, current) => prev + current); // Sum
+    .reduce((Σ, σ) => Σ + σ); // Sum
 }
 
 /**
@@ -63,6 +63,6 @@ export function int_4(
 
   // deno-fmt-ignore
   return (2/45)*h*range(1, n/4)
-    .map((i) => 7*f(x(4*i - 4)) + 32*f(x(4*i - 3)) + 12*f(x(4*i - 2)) + 32*f(x(4*i - 1)) + 7*f(x(4*i)))
-    .reduce((prev, current) => prev + current); // Sum
+    .map(i => 7*f(x(4*i - 4)) + 32*f(x(4*i - 3)) + 12*f(x(4*i - 2)) + 32*f(x(4*i - 1)) + 7*f(x(4*i)))
+    .reduce((Σ, σ) => Σ + σ); // Sum
 }
